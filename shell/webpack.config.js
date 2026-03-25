b18cb3aa90c7e4cf8c38c6d5267c6cdbfe85146f
Amit Kumar Sharma <B0335420@airtel.com>
2026-03-26 02:18:09 +0530

    back to basics

--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -71,7 +71,7 @@ module.exports = {
         { from: 'public/favicon-32x32.png', to: 'favicon-32x32.png' },
         { from: 'public/favicon-16x16.png', to: 'favicon-16x16.png' },
         { from: 'public/apple-touch-icon.png', to: 'apple-touch-icon.png' },
-        { from: 'public/manifest.json', to: 'manifest.json' },
+        { from: 'public/site.webmanifest', to: 'site.webmanifest' },
         { from: 'public/android-chrome-192x192.png', to: 'android-chrome-192x192.png' },
         { from: 'public/android-chrome-512x512.png', to: 'android-chrome-512x512.png' },
       ],
