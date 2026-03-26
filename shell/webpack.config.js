5ba8776585481c95f033b4ed9be7a5ec11526d25
Amit Kumar Sharma <B0335420@airtel.com>
2026-03-26 23:21:31 +0530

    changed copyplugin

--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -65,14 +65,15 @@ module.exports = {
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'public/_redirects', to: '_redirects' },
-        { from: 'public/robots.txt', to: 'robots.txt' },
-        { from: 'public/favicon-32x32.png', to: 'favicon-32x32.png' },
-        { from: 'public/favicon-16x16.png', to: 'favicon-16x16.png' },
-        { from: 'public/apple-touch-icon.png', to: 'apple-touch-icon.png' },
-        { from: 'public/site.webmanifest', to: 'site.webmanifest' },
+        {
+          from: 'public',
+          to: '',
+          globOptions: {
+            ignore: ['**/index.html'], // ✅ THIS FIXES IT
+          },
+        },
       ],
-    }),
+    })
   ],
   devServer: {
     port: 4001,
