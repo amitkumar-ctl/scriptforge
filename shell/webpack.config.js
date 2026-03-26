ae36423846cb68614498e4715696026282111728
Amit Kumar Sharma <B0335420@airtel.com>
2026-03-26 22:34:20 +0530

    changed some configuration of cloudflare

--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -67,13 +67,10 @@ module.exports = {
       patterns: [
         { from: 'public/_redirects', to: '_redirects' },
         { from: 'public/robots.txt', to: 'robots.txt' },
-        { from: 'public/favicon.ico', to: 'favicon.ico' },
         { from: 'public/favicon-32x32.png', to: 'favicon-32x32.png' },
         { from: 'public/favicon-16x16.png', to: 'favicon-16x16.png' },
         { from: 'public/apple-touch-icon.png', to: 'apple-touch-icon.png' },
         { from: 'public/site.webmanifest', to: 'site.webmanifest' },
-        { from: 'public/android-chrome-192x192.png', to: 'android-chrome-192x192.png' },
-        { from: 'public/android-chrome-512x512.png', to: 'android-chrome-512x512.png' },
       ],
     }),
   ],
